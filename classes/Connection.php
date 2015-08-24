be1b3a2bfa037fc120ec7178f44cc11b2039ddf9
Uğur Arıcı <m.ugurarici@gmail.com>
2015-08-24 23:57:08 +0300

    Connection class update for settings and exception

--- classes/Connection.php
+++ classes/Connection.php
@@ -3,8 +3,16 @@
 class Connection
 {
     protected $con;
+	private $host = "localhost";
+	private $dbname = "restaurant";
+	private $username = "root";
+	private $password = "";
 
     function __construct(){
-        $this->con = new PDO("mysql:host=localhost;dbname=restaurant;charset=UTF8;", "root", "");
+		try{
+			$this->con = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname.";charset=UTF8;", $this->username, $this->password);
+		}catch (PDOException $e){
+			die("Veritabani baglanti hatasi: ".$e->getMessage());
+		}
     }
 }
