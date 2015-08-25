39bb6af5e14f5682b5f92a8b16b95a278684520a
Ömer İPEK <wb_omer@hotmail.com>
2015-08-25 15:02:09 +0300

    Mobile meta tag ve zoom yapılmasını engelleme
    Sadece büyük ekranlarda çalışılmayacağı için responsive aktif olması iyi olur. Ayrıca sistem yapısı gereği zoom yapılmasını iptal etmemiz daha doğru olurdu. Yanlışlıkla çift tıklamalar vs olabilirdi...

--- view/_header.php
+++ view/_header.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Restaurant</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <!-- "user-scalable=no" ile zoom yapma özelliğini, proje yapısında sorun olabileceğini düşünerek iptal ettim. -->
     <link rel="stylesheet" href="<?=$sitePath?>assets/css/bootstrap.min.css">
     <script src="<?=$sitePath?>assets/js/jquery-1.7.2.min.js"></script>
     <script src="<?=$sitePath?>assets/js/site.js"></script>
