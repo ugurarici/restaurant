93a0d8fc9eb916141abdf821f5847167b9ff3198
MelikeTopcu <MelikeTopcu@users.noreply.github.com>
2015-08-26 13:08:23 +0300

    Create first.php

---
+++ view/first.php
@@ -0,0 +1,22 @@
+<br>
+
+<form action="login.php" method="POST">
+    <table align="center">
+        <tr>
+            <td>Kullanici Adi</td>
+            <td>:</td>
+            <td><input type="text" name="kadi"></td>
+        </tr>
+        <tr>
+            <td>Sifre</td>
+            <td>:</td>
+            <td><input type="password" name="sifre"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><input type="submit" value="Giris"></td>
+
+        </tr>
+    </table>
+</form>
