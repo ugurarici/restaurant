e7408757459a66253c5a9d840f7aa7d95e1ee501
Ömer İPEK <wb_omer@hotmail.com>
2015-08-28 09:31:28 +0300

    utf8 'e çevrildi

--- ayar.php
+++ ayar.php
@@ -6,5 +6,5 @@ $pass="";
 $conn=@mysql_connect($host,$user,$pass) or die("Mysql Baglanamadi");
 
 mysql_select_db($db,$conn) or die("Veritabanina Baglanilamadi");
-mysql_set_charset('latin5',$conn);
+mysql_query("Set names 'utf8'");
 ?>
