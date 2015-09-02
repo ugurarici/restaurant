7af8ec7c5fce8cf2b96e7d62bf882737ca2ed2f3
Ömer İPEK <wb_omer@hotmail.com>
2015-09-02 16:34:19 +0300

    login page

---
+++ view/login.php
@@ -0,0 +1,54 @@
+<div class="col-sm-6 centered">
+    <h1>Personel Girişi</h1>
+    <form action="<?=$sitePath?>userTasks.php" method="POST">
+        <?php
+        if(isset ($_GET['error']) == "login")
+        {
+            ?>
+            <div class="alert alert-danger">
+                <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo "Ama öyle giriş yapmayacaktınız! Lütfen tekrar deneyiniz!"; ?> !
+            </div>
+        <?php
+        }
+        ?>
+
+        <table class="table table-bordered table-striped">
+            <thead>
+            <tr class="bg-success">
+                <th colspan="2">Personel Yetki Girişi Alanı</th>
+            </tr>
+            </thead>
+            <tbody class="searchOrder">
+            <tr>
+                <td>
+                    Kullanıcı Adı / e-mail:
+                </td>
+                <td class="text-center">
+                    <label>
+                        <input type="text" name="username" class="form-control" required/>
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Şifre:
+                </td>
+                <td class="text-center ">
+                    <label>
+                        <input type="password" name="password" class="form-control" required/>
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td class="text-center">
+                    <label>
+                        <input class="btn btn-warning" type="submit" name="submit" value="Yetkili Girişi Yap"/>
+                    </label>
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+    </form>
+</div>
