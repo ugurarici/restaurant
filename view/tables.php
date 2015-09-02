5c53de9dc92cc117eb111a20acec841e274f3ef0
Ömer İPEK <wb_omer@hotmail.com>
2015-09-02 16:40:18 +0300

    Update tables.php

--- view/tables.php
+++ view/tables.php
@@ -1,5 +1,30 @@
 <div class="container">
-    <div class="row text-center"><a href="products.php" class="btn btn-warning">Ürün Yönetimi</a></div>
+    <?php
+        $userId = $_SESSION['user_session'];
+        $userInfo = $usrObj->getOneUser($userId);
+        if($userInfo["user_position"] == 1):
+        ?>
+        <div class="row text-center">
+            <p class="pull-left"> Hoşgeldin <b><?=$userInfo["fullname"]; ?>,</b></p>
+            <a href="admin.php" class="btn btn-warning">Admin Paneli</a>
+            <a href="products.php" class="btn btn-warning">Ürün Yönetimi</a>
+            <label class="pull-right"><a href="<?=$sitePath?>logout.php?logOut=true"><i class="glyphicon glyphicon-log-out"></i> Çıkış Yap</a></label>
+        </div>
+
+        <?php
+         elseif ($userInfo["user_position"] == 2):
+        ?>
+        <div class="row text-center"><a href="products.php" class="btn btn-warning">Ürün Yönetimi</a></div>
+        <label class="pull-right"><a href="<?=$sitePath?>logout.php?logOut=true"><i class="glyphicon glyphicon-log-out"></i> Çıkış Yap</a></label>
+        <?php
+        else:
+        ?>
+            <label class="pull-right"><a href="<?=$sitePath?>logout.php?logOut=true"><i class="glyphicon glyphicon-log-out"></i> Çıkış Yap</a></label>
+        <?php
+        endif;
+        ?>
+
+
     <hr />
     <?php
     foreach ($tblObj->getAllTables() as $table):
